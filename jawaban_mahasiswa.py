2f2ec49b84653a52c1ed890d695a109834531f23
dhans24 <fastgoole@gmail.com>
2026-06-13 19:16:06 +0700

    Update jawaban_mahasiswa.py

--- jawaban_mahasiswa.py
+++ jawaban_mahasiswa.py
@@ -1,1 +1,8 @@
-
+# File: jawaban_mahasiswa.py (Solusi Pemula)
+def cari_pasangan(arr, target):
+    # Menggunakan perulangan bersarang (Kompleksitas O(N^2))
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            if arr[i] + arr[j] == target:
+                return True
+    return False
